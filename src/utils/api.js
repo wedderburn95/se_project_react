e33f6b33864ab1394865ae0c9b557ed92f51bfd8
William <wedderburn95@yahoo.com>
2025-05-04 21:32:16 -0400

    Created a new checkResponse function to handle the responses

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 const baseUrl = "http://localhost:3001";
 
-function checkResponse(res) {
+export function checkResponse(res) {
   if (res.ok) {
     return res.json();
   }
--- src/utils/weatherApi.js
+++ src/utils/weatherApi.js
@@ -1,13 +1,9 @@
+import { checkResponse } from "./api.js";
+
 export const getWeatherData = ({ latitude, longitude }, APIkey) => {
   return fetch(
     `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${APIkey}`
-  ).then((res) => {
-    if (res.ok) {
-      return res.json();
-    } else {
-      return Promise.reject(`Error : ${res.status}`);
-    }
-  });
+  ).then(checkResponse);
 };
 
 export const filterWeatherData = (data) => {
